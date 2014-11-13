efb0fea521f35fc40f74435ebbac57f97a432982
rfyodorov <r.fyodorov@gmail.com>
2014-11-13 19:27:54 +0400

    rewrite with pointers

--- 2.4/step7.cpp
+++ 2.4/step7.cpp
@@ -2,6 +2,33 @@
 
 void strcat(char *to, const char *from) {
     // put your code here
+    while(*to) {
+        to++;
+    }
+
+    while(*from) {
+        *to = *from;
+	to++;
+	from++;
+    }
+
+    *to = '\0';
+
+}
+
+
+int main() {
+    char str1[24] = "Hello,";
+    const char str2[] = " World!";
+
+    strcat(str1, str2);
+    return 0;
+}
+
+/* // first version
+
+void strcat(char *to, const char *from) {
+    // put your code here
     int i =0;
     int j =0;
 
@@ -14,23 +41,10 @@ void strcat(char *to, const char *from) {
     }
 
     for(int k=0; k<j; k++) {
-        to[i] = from[k];        
+        to[i] = from[k];
         i=i++;
     }
-
     to[i] = '\0';
-
-    std::cout << to << std::endl;
 }
 
-
-int main() {
-    char str1[14] = "Hello,";
-    const char str2[] = " World!";
-
-    strcat(str1, str2);
-    return 0;
-}
-
-
-
+*/
